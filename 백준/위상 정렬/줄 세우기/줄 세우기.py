3cf437a6b340c41a8842f022ef3cef2c221d51e3
rlaehgud52 <82474786+rlaehgud52@users.noreply.github.com>
2025-02-15 21:12:09 +0900

    Update 줄 세우기.py

--- 백준/위상 정렬/줄 세우기/줄 세우기.py
+++ 백준/위상 정렬/줄 세우기/줄 세우기.py
@@ -1,17 +1,20 @@
 import sys
 input=sys.stdin.readline
+ans=[]
 n,m=map(int,input().split())
+
+# 간선그래프와 피간선개수 표 작성
 g=[[] for _ in range(n+1)]
 cnt=[0]*(n+1)
 for _ in range(m):
   a,b=map(int,input().split())
   g[a].append(b)
   cnt[b]+=1
-ans=[]
 
 from collections import deque
 q=deque()
 
+# 피간선개수가 0인 지점 전부 삽입
 for i in range(1,n+1):
   if cnt[i]==0:
     q.append(i)
@@ -21,6 +24,7 @@ while q:
     ans.append(p)
     for i in g[p]:
       cnt[i]-=1
+      # 피간선개수가 0이 되면 바로 삽입해주는 것이 포인트
       if cnt[i]==0:
             q.append(i)
       
